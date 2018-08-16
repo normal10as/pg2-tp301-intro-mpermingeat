3bd036c51b94391392c9dff632a63b7e5cdf6bf8
maxpermin <maxpermin@outlook.com>
2018-08-15 21:57:10 -0300

    1

---
+++ MAscotas/ClassLibrary1/Loro.vb
@@ -0,0 +1,45 @@
+﻿Public Class Loro
+
+    Private _nombre As String
+    Private _fechaNacimiento As Date
+    Private _edad As Short
+    Private _memoria As New Queue
+
+    '////////// Properties /////////////////////
+    Public Property Nombre() As String
+        Get
+            Return _nombre
+        End Get
+        Set(ByVal value As String)
+            _nombre = value
+        End Set
+    End Property
+
+    Public Property FechaNacimiento() As Date
+        Get
+            Return _fechaNacimiento
+        End Get
+        Set(ByVal value As Date)
+            _fechaNacimiento = value
+        End Set
+    End Property
+
+    Public ReadOnly Property Edad() As Short
+        Get
+            Return _edad
+        End Get
+
+    End Property
+
+    Private Property Memoria() As Queue
+        Get
+            Return _memoria
+        End Get
+        Set(ByVal value As Queue)
+            _memoria = value
+        End Set
+    End Property
+
+    '////////// Metodos /////////////////////
+
+End Class
---
+++ MAscotas/MAscotas/Mascota.vb
@@ -0,0 +1,7 @@
+﻿Module Mascota
+
+    Sub Main()
+
+    End Sub
+
+End Module
